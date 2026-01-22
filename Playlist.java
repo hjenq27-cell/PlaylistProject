042b3b5d1c5f21e73022b24be11ea0647bdb9aa0
Hudson Jenq <hjenq27@students.polytechnic.org>
2026-01-22 10:00:25 -0800

    Branch test

--- Playlist.java
+++ Playlist.java
@@ -18,7 +18,10 @@ public class Playlist {
       * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
       * then use additional methods to add Songs in one-by-one
       */
+    public Playlist(){
 
+
+    }
 
       
 
